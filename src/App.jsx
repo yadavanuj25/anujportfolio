3e80fef24b6cd2412dd2ce0c75b82b0c97363199
Anuj2508 <anuj.y@ecodedash.com>
2025-09-14 15:18:50 +0530

    Preloader issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,29 @@
+// import React, { useState, useEffect } from "react";
+// import AppRouter from "./routers/AppRouter";
+// import PreLoader from "./components/loaders/PreLoader";
+
+// const App = () => {
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const handleLoad = () => {
+//       setLoading(false);
+//     };
+
+//     window.addEventListener("load", handleLoad);
+
+//     return () => window.removeEventListener("load", handleLoad);
+//   }, []);
+
+//   return (
+//     <main className="overflow-x-hidden bg-white text-dark">
+//       {loading ? <PreLoader /> : <AppRouter />}
+//     </main>
+//   );
+// };
+
+// export default App;
+
 import React, { useState, useEffect } from "react";
 import AppRouter from "./routers/AppRouter";
 import PreLoader from "./components/loaders/PreLoader";
@@ -6,13 +32,7 @@ const App = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const handleLoad = () => {
-      setLoading(false);
-    };
-
-    window.addEventListener("load", handleLoad);
-
-    return () => window.removeEventListener("load", handleLoad);
+    setLoading(false); // App is mounted, hide preloader
   }, []);
 
   return (
